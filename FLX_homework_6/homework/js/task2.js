d93c0097008d38f21d902990da1d2c79052353bc
Iryna Sorokolita <46350951+irynasorokolita@users.noreply.github.com>
2019-02-05 21:09:39 +0200

    Update task2.js

--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,13 +1,12 @@
-
-    let price = parseFloat(prompt('Input amount of money'));
-    let discount = parseFloat(prompt('Input discount'));
-    if (price >= 0 && price <= 9999999 && discount >= 0 && discount <= 99) {
-        let priceWithDiscount = price * (100 - discount) / 100;
-        let saved = price - priceWithDiscount;
-        alert('Price without discount: ' + +price.toFixed(2) +
-        '\nDiscount: ' + +discount.toFixed(2) + '%' +
-        '\nPrice with discount: ' + +priceWithDiscount.toFixed(2) +
-        '\nSaved: ' + +saved.toFixed(2));
-    } else {
+let price = parseFloat(prompt('Input amount of money'));
+let discount = parseFloat(prompt('Input discount'));
+if (price >= 0 && price <= 9999999 && discount >= 0 && discount <= 99) {
+    let priceWithDiscount = price * (100 - discount) / 100;
+    let saved = price - priceWithDiscount;
+    alert('Price without discount: ' + +price.toFixed(2) +
+    '\nDiscount: ' + +discount.toFixed(2) + '%' +
+    '\nPrice with discount: ' + +priceWithDiscount.toFixed(2) +
+    '\nSaved: ' + +saved.toFixed(2));
+} else {
         alert('Invalid input data');
-    }
+}
